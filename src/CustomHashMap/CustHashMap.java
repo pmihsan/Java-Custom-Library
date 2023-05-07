0f4061578cefb4f86acf13929274a9a6ac18c3ba
Mohamed Ihsan <89512452+pmihsan@users.noreply.github.com>
2023-05-07 12:06:04 +0530

    Added Custom Hash Map

--- src/CustomHashMap/CustHashMap.java
+++ src/CustomHashMap/CustHashMap.java
@@ -1,4 +1,119 @@
 package CustomHashMap;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class CustHashMap {
+    class keyPair implements Comparable<keyPair> {
+        private int key, val;
+
+        keyPair(){
+            key = -1;
+            val = -1;
+        }
+        keyPair(int k, int v){
+            key = k;
+            val = v;
+        }
+        public int getKey() {
+            return key;
+        }
+        public int getValue() {
+            return val;
+        }
+        public void setValue(int v) {
+            val = v;
+        }
+
+        public int compareTo(keyPair kp){
+            if(this.key > kp.getKey()) return 1;
+            else if(this.key < kp.getKey()) return -1;
+            return 0;
+        }
+    }
+
+    private int size;
+    private int sortSize;
+    List<keyPair> l;
+
+    public CustHashMap(){
+        this.size = 0;
+        this.sortSize = 0;
+        this.l = new ArrayList<>();
+    }
+
+    public int getSize(){
+        return size;
+    }
+
+
+    public int hashCode(){
+        int hashcode = -1;
+        for(keyPair kp : l){
+                hashcode += (kp.hashCode());
+        }
+        return hashcode;
+    }
+
+    public keyPair search(int key){
+        for(int i=0;i<size;i++){
+            if(l.get(i).getKey() == key) return l.get(i);
+        }
+        return new keyPair();
+    }
+
+    public void put(int key, int val){
+        if(!containsKey(key)) {
+            keyPair temp = new keyPair(key, val);
+            l.add(temp);
+            this.size++;
+        }
+        else{
+            updateKey(key, val);
+        }
+    }
+
+    public void remove(int key){
+        keyPair temp = search(key);
+        if(temp.getKey() != -1) {
+            l.remove(temp);
+            this.size--;
+        }
+    }
+
+    public boolean containsKey(int key){
+        keyPair temp = search(key);
+        if(temp.getKey() != -1) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean updateKey(int key, int data){
+        keyPair temp = search(key);
+        if(temp.getKey() != -1) {
+            temp.setValue(data);
+            return true;
+        }
+        return false;
+    }
+
+    public String toString(){
+        if(size != sortSize) {
+            Collections.sort(l);
+            this.sortSize = this.size;
+        }
+        String s = "{";
+        if(size > 0) {
+            for (int i = 0; i < size-1; i++) {
+                if(i == 0) s += " ";
+                s += l.get(i).getKey() + "=" + l.get(i).getValue() + ", ";
+            }
+            s += l.get(size - 1).getKey() + "=" + l.get(size - 1).getValue();
+        }
+        return s + " }";
+    }
+
+
 }
--- src/CustomHashMap/Main.java
+++ src/CustomHashMap/Main.java
@@ -1,12 +1,42 @@
 package CustomHashMap;
 
-import java.util.*;
-
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, String> hm = new HashMap<>();
-        LinkedHashMap<String, String> lhm = new LinkedHashMap<>();
-        HashSet<String> hs = new HashSet<>();
-        TreeMap<String, Integer> m = new TreeMap<>();
+        CustHashMap chm = new CustHashMap();
+
+        System.out.println("Size " + chm.getSize());
+        System.out.println("Hashcode " + chm.hashCode());
+        System.out.println(chm);
+        System.out.println();
+
+        chm.put(23,45);
+        chm.put(45,36);
+        chm.put(12, 70);
+
+        System.out.println("Size " + chm.getSize());
+        System.out.println("Hashcode " + chm.hashCode());
+        System.out.println(chm);
+        System.out.println();
+
+        chm.put(12, 90);
+        chm.put(10, 22);
+        System.out.println("Size " + chm.getSize());
+        System.out.println("Hashcode " + chm.hashCode());
+        System.out.println(chm);
+        System.out.println();
+
+        if(chm.containsKey(12)) System.out.println("FOUND 12");
+        else System.out.println("NOT FOUND");
+
+        chm.updateKey(10, 2222);
+        System.out.println(chm);
+        System.out.println();
+
+        chm.remove(10);
+        chm.remove(45);
+        System.out.println("Size " + chm.getSize());
+        System.out.println("Hashcode " + chm.hashCode());
+        System.out.println(chm);
+
     }
 }
